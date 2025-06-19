0f7d0957a5f1697d6f0594873f7a7b944993c892
al8032pa <alexandros.pallaris@eit.lth.se>
2025-06-19 14:57:02 +0200

    3

--- Scatt3D/jobscript.sh
+++ Scatt3D/jobscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # ### SBATCH # --qos=test  ## to run a quick, high-priority test (should be very quick). This is currently commented out (I hope)
-#SBATCH -t 12:45:00 ## job is killed after this time - overestimate
+#SBATCH -t 1:45:00 ## job is killed after this time - overestimate
 #SBATCH -A lu2024-2-93 ##from projinfo command
 
 #SBATCH -N 1 ##number of nodes that will be allocated - must use --ntasks-per-node or --cpus-per-task to use more than 1 core per node
-#SBATCH --tasks-per-node=24 ##number of cores used per task? - up to 48 per node for COSMOS. Presumably this is what I want, using MPI
+#SBATCH --tasks-per-node=48 ##number of cores used per task? - up to 48 per node for COSMOS. Presumably this is what I want, using MPI
 
 #SBATCH -o jobresults/%j.out ## result filename, %j becomes the job number
 #SBATCH -e jobresults/%j.err ## errors filename - should be empty unless an error occurs
