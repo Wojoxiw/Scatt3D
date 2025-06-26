7fea02a99de573a650bd6e9bb6c36c9bde9f9317
al8032pa <alexandros.pallaris@eit.lth.se>
2025-06-26 10:55:46 +0200

    1go

--- Scatt3D/runScatt3D.py
+++ Scatt3D/runScatt3D.py
@@ -125,7 +125,7 @@ if __name__ == '__main__':
  
     def convergenceTestPlots(convergence = 'meshsize', deg=1): ## Runs with reducing mesh size, for convergence plots. Uses the far-field surface test case. If showPlots, show them - otherwise just save them
         if(convergence == 'meshsize'):
-            ks = np.linspace(6, 14, 13)
+            ks = np.linspace(4, 28, 13)
         elif(convergence == 'pmlR0'): ## result of this is that the value must be below 1e-2, from there further reduction matches the forward-scattering better, the back-scattering less
             ks = np.linspace(2, 15, 10)
             ks = 10**(-ks)
@@ -293,7 +293,7 @@ if __name__ == '__main__':
     #testFullExample(h=1/16)
     #testSphereScattering(h=1/7, degree=1, showPlots=True)
     #convergenceTestPlots('pmlR0')
-    convergenceTestPlots('meshsize', deg=2)
+    convergenceTestPlots('meshsize', deg=1)
     #convergenceTestPlots('dxquaddeg')
     #testSolverSettings(h=1/15)
     
