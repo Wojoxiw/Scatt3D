7f1724c8532103880267928f2ff2d5f2ad1c5b35
al8032pa <alexandros.pallaris@eit.lth.se>
2025-10-03 15:25:39 +0200

    22

--- Scatt3D/runScatt3D.py
+++ Scatt3D/runScatt3D.py
@@ -381,12 +381,12 @@ if __name__ == '__main__':
             plt.show()
     
     
-    runName = 'testRunDeg1'
+    runName = 'testRunDeg2'
     
     #testRun(h=1/3)
     #profilingMemsTimes()
     #actualProfilerRunning()
-    testFullExample(h=1/17.5, degree=1)
+    testFullExample(h=1/9.3, degree=1)
     #testSphereScattering(h=1/6, degree=2, showPlots=True)
     #convergenceTestPlots('pmlR0')
     #convergenceTestPlots('meshsize', deg=3)
