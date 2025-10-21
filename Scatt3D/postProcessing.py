8d791838201b553b666ed376a680ce2af25547ac
al8032pa <alexandros.pallaris@eit.lth.se>
2025-10-21 12:06:56 +0200

    cvx2

--- Scatt3D/postProcessing.py
+++ Scatt3D/postProcessing.py
@@ -101,9 +101,9 @@ def testSolverSettings(A, b, epsr_ref, epsr_dut, cell_volumes): # Varies setting
     settings = [] ## list of solver settings
     
     ## MG tests
-    testName = 'cvxpy_type1_testsolvers'
+    testName = 'cvxpy_type2_testsolvers'
     for solver in cp.installed_solvers():
-        settings.append( {'solver': solver, 'solveType': 1} )
+        settings.append( {'solver': solver, 'solveType': 2} )
     
                             
     num = len(settings)
