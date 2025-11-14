c10c6f047a590f265e52b697d9430531f9812cd5
al8032pa <alexandros.pallaris@eit.lth.se>
2025-11-14 13:45:22 +0100

    pac

--- Scatt3D/runScatt3D.py
+++ Scatt3D/runScatt3D.py
@@ -500,8 +500,8 @@ if __name__ == '__main__':
     
     
     
-    errorTestPlots()
-    errorTestPlots(False)
+    #errorTestPlots()
+    #errorTestPlots(False)
     
     #runName = 'testRunDeg2' ## h=1/9.5
     #runName = 'testRunDeg2Smaller' ## h=1/6
@@ -525,9 +525,9 @@ if __name__ == '__main__':
     #convergenceTestPlots('dxquaddeg')
     #testSolverSettings(h=1/6)
     
-    #runName = 'patchPatternTest'
-    #testPatchPattern(h=1/4, degree=3, freqs = np.linspace(8e9, 12e9, 30))
-    #postProcessing.solveFromQs(folder+runName, solutionName='', onlyAPriori=True)
+    runName = 'patchPatternTest'
+    testPatchPattern(h=1/4, degree=3, freqs = np.linspace(8e9, 12e9, 30))
+    postProcessing.solveFromQs(folder+runName, solutionName='', onlyAPriori=True)
     
     #===========================================================================
     # runName = 'testingComplexObject' ## h=1/12
