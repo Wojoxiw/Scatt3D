2501429387de3a9d3580bea0756e473f62c161db
al8032pa <alexandros.pallaris@eit.lth.se>
2026-01-13 11:53:24 +0100

    upd patch

--- Scatt3D/meshMaker.py
+++ Scatt3D/meshMaker.py
@@ -180,13 +180,13 @@ class MeshInfo():
         self.antenna_height = antenna_height * self.lambda0
         self.antenna_depth = antenna_depth * self.lambda0
         if(self.antenna_type == 'patch' or self.antenna_type == 'patchtest'): ## specify the dimensions here, for a patch active near 10 GHz
-            self.antenna_width = 26e-3 ## the width
             self.antenna_height = 1e-3 ## the height
-            self.antenna_depth = 18e-3 ## the length (in x)
-            self.patch_length = 9e-3
-            self.patch_width = 13e-3
+            self.patch_length = 9.056e-3
+            self.patch_width = 13.518e-3
+            self.antenna_width = self.patch_width*2 ## the width
+            self.antenna_depth = self.patch_length*2 ## the length (in x)
             self.coax_inr = .65e-3; self.coax_outr = 2.1e-3; self.coax_outh = 1e-3 ## coaxial inner and outer radii, and the height it extends beyond the substrate
-            self.feed_offset = 2e-3
+            self.feed_offset = 2.697e-3
             
         if(antenna_bounding_box_offset == 0): ## if unset
             self.bb = min(h/2, self.lambda0/10)
