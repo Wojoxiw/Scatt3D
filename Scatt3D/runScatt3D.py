7848ef4afc2d14bfe782a17785b7d704af375c92
al8032pa <alexandros.pallaris@eit.lth.se>
2026-02-02 10:55:47 +0100

    qsview2

--- Scatt3D/runScatt3D.py
+++ Scatt3D/runScatt3D.py
@@ -806,7 +806,7 @@ if __name__ == '__main__':
     # runName = 'testRunComplex2Obj'
     # testFullExample(h=1/3.5, degree=3, runName=runName,
     #                 mesh_settings={ 'viewGMSH': False, 'N_antennas': 9, 'antenna_type': 'patch', 'object_geom': 'complex2', 'defect_geom': 'complex2', 'defect_radius': 0.475, 'object_radius': 5, 'domain_radius': 4.5, 'domain_height': 1.5, 'object_offset': np.array([.15, .1, 0]), 'defect_offset': np.array([-.04, .17, 0])},
-    #                 prob_settings={'Nf': 10, 'defect_epsrs': [2.0*(1 - 0.01j), 4.0*(1 - 0.01j), 3.3*(1 - 0.01j)]})
+    #                 prob_settings={'Nf': 13, 'defect_epsrs': [2.0*(1 - 0.01j), 4.0*(1 - 0.01j), 3.3*(1 - 0.01j)]})
     #===========================================================================
     
     #===========================================================================
@@ -833,7 +833,7 @@ if __name__ == '__main__':
     runName = 'testRunD3LowerContrastQsView'
     testFullExample(h=1/3.5, degree=3, runName=runName, recMesh=False,
                     mesh_settings={'viewGMSH': False, 'N_antennas': 9, 'antenna_type': 'patch', 'object_geom': 'simple1', 'defect_geom': 'simple1', 'defect_radius': 0.475, 'object_radius': 4, 'domain_radius': 3, 'domain_height': 1.3, 'object_offset': np.array([.15, .1, 0]), 'defect_offset': np.array([-.04, .17, 0])},
-                    prob_settings={'Nf': 26, 'material_epsrs' : [3*(1 - 0.01j)], 'defect_epsrs' : [3.1*(1 - 0.01j)]})
+                    prob_settings={'Nf': 13, 'material_epsrs' : [3*(1 - 0.01j)], 'defect_epsrs' : [3.1*(1 - 0.01j)]})
     
     postProcessing.solveFromQs(folder+runName, solutionName='', onlyAPriori=True)#, frequenciesToUse=[2, 4, 6, 8, 12, 14, 16, 18, 20, 22], returnResults=[3, 25])
     
