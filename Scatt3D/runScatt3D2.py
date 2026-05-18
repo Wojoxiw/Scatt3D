cd4f77cea54d9cd2d233fe8db6138df36daf72da
al8032pa <alexandros.pallaris@eit.lth.se>
2026-05-18 15:13:12 +0200

    tryref

--- Scatt3D/runScatt3D2.py
+++ Scatt3D/runScatt3D2.py
@@ -247,17 +247,17 @@ if __name__ == '__main__':
     
     #runName = f'measurements_init_'
     #runName = f'measurements_init_actuallMeasuredFreqs_'
-    #runName = f'measurements_corrected_'
-    runName = f'measurements_corrected_smallmesh_' ## made it to 240 degrees before seeming to time out
+    runName = f'measurements_corrected_'
+    #runName = f'measurements_corrected_smallmesh_' ## made it to 240 degrees before seeming to time out
+    #runName = f'measurements_corrected_semismallmesh_' ## using new FR4 epsr=4.3
+    
     angles = np.arange(0, 360, 40, dtype=float) ## measured with 20-degree spacing, simulate 40 degree so its faster
     measFreqs = np.linspace(5.4e9, 7.2e9, 201) ## the measured frequencies
     freqs = [measFreqs[i] for i in np.arange(len(measFreqs)) if i%10==0] ## simulate these 21 frequencies
     
-    #===========================================================================
-    # measurementScript(h=1/5, degree=3, runName=runName, angles=angles,
-    #                 mesh_settings={'viewGMSH': True, 'N_antennas': 4, 'f0': 6e9, 'antenna_type': '6GHz measurement', 'antenna_radius': 0.18, 'object_geom': '6GHz measurement', 'domain_height': 1, 'domain_radius': 4.2},
-    #                 prob_settings={'freqs': freqs, 'material_epsrs' : [2.73 - .014j]}) # epsr of POM taken from Complex Permittivity Measurements of Common Plastics Over Variable Temperatures, Bill Riddle
-    #===========================================================================
+    measurementScript(h=1/4, degree=3, runName=runName, angles=angles,
+                    mesh_settings={'viewGMSH': False, 'N_antennas': 4, 'f0': 6e9, 'antenna_type': '6GHz measurement', 'antenna_radius': 0.18, 'object_geom': '6GHz measurement', 'domain_height': 1, 'domain_radius': 4.2},
+                    prob_settings={'freqs': freqs, 'material_epsrs' : [2.73 - .014j]}) # epsr of POM taken from Complex Permittivity Measurements of Common Plastics Over Variable Temperatures, Bill Riddle
     
     testrunName = f'measurements_noobject'
     #===========================================================================
@@ -273,11 +273,11 @@ if __name__ == '__main__':
     #                 prob_settings={'freqs': freqs, 'material_epsrs' : [2.73 - .014j], 'defect_epsrs' : [1.0 - .0j]}) # epsr of POM taken from Complex Permittivity Measurements of Common Plastics Over Variable Temperatures, Bill Riddle
     #===========================================================================
     
-    testrunName = f'{runName}dut_2.5fill_' ## the test case where there is a hole totally filled with a epsr=2.5 cylinder
+    testrunName = f'{runName}dut_2.8fill_' ## the test case where there is a hole totally filled with a epsr=2.5 cylinder
     #===========================================================================
-    # measurementScript(h=1/3.5, degree=3, runName=testrunName, angles=angles, dutForSimSolution=True,
+    # measurementScript(h=1/4, degree=3, runName=testrunName, angles=angles, dutForSimSolution=True,
     #                 mesh_settings={'viewGMSH': False, 'N_antennas': 4, 'f0': 6e9, 'antenna_type': '6GHz measurement', 'antenna_radius': 0.18, 'object_geom': '6GHz measurement', 'defect_geom': '6GHz measurement cyl fill', 'domain_height': 1, 'domain_radius': 4.2},
-    #                 prob_settings={'freqs': freqs, 'material_epsrs' : [2.73 - .014j], 'defect_epsrs' : [2.5*(1 - .01j)]}) # epsr of POM taken from Complex Permittivity Measurements of Common Plastics Over Variable Temperatures, Bill Riddle
+    #                 prob_settings={'freqs': freqs, 'material_epsrs' : [2.73 - .014j], 'defect_epsrs' : [2.8*(1 - .01j)]}) 
     #===========================================================================
     
     ## try the postprocessing with just sim. stuff:
@@ -321,8 +321,8 @@ if __name__ == '__main__':
     #testPatchPattern(h=1/6, name=f'6GHzpatchPatternTest_aftermeas_ho{6.0:.1f}_epsr4.3', epsr_FR4=4.3*(1-.11/4.4j), degree=3, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
     #testPatchPattern(h=1/6, name=f'6GHzpatchPatternTest_aftermeas_ho{6.0:.1f}_epsr4.2', epsr_FR4=4.2*(1-.11/4.4j), degree=3, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
     
-    testPatchPattern(h=1/20, name=f'6GHzpatchPatternTest_deg1_ho{20.0:.1f}', epsr_FR4=4.3*(1-.11/4.4j), degree=1, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
-    testPatchPattern(h=1/6, name=f'6GHzpatchPatternTest_order2mesh_ho{6.0:.1f}', epsr_FR4=4.3*(1-.11/4.4j), degree=3, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
+    #testPatchPattern(h=1/20, name=f'6GHzpatchPatternTest_deg1_ho{20.0:.1f}', epsr_FR4=4.3*(1-.11/4.4j), degree=1, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
+    #testPatchPattern(h=1/6, name=f'6GHzpatchPatternTest_order2mesh_ho{6.0:.1f}', epsr_FR4=4.3*(1-.11/4.4j), degree=3, freqs = np.linspace(5.4e9, 6.6e9, 22), showPlots=False)
     
     #patchSsPlot([f'6GHzpatchPatternTest_largerdomain_ho{5.0:.1f}_epsr4.3', f'6GHzpatchPatternTest_deg1_ho{20.0:.1f}', f'6GHzpatchPatternTest_order2mesh_ho{6.0:.1f}]) ## plot S11 comp. with Feko
     
